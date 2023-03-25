202dbc1030b6ec19ed93e7aca6c9db50dd42a342
Abhineet Singh <baghelabhicoc7@gmail.com>
2023-03-25 15:11:21 +0530

    updated mama-shooter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import mamaShooterImage from './rick.png';
+
+function MamaShooter() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', height: '100%' }}>
+      <img src={mamaShooterImage} alt="Mama Shooter" style={{ width: '60%', maxWidth: '800px', marginBottom: '20px' }} />
+      <h1>mama-shooter</h1>
+      <p>Check out the game here:</p>
+      <a href="https://immortalll.itch.io/mama-shooter-web3" target="_blank">mama-shooter-web3</a>
+    </div>
+  );
+}
+
+export default MamaShooter;
